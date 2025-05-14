e3d47f2f219e6c13244fd43fd944cc5ec55a21c8
Mouiz Ahmed <mouiza@my.yorku.ca>
2025-05-14 17:53:30 -0400

    Initial Commit

---
+++ buzzr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  buzzr
+//
+//  Created by M . on 2025-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ buzzr/buzzrApp.swift
@@ -0,0 +1,17 @@
+//
+//  buzzrApp.swift
+//  buzzr
+//
+//  Created by M . on 2025-05-14.
+//
+
+import SwiftUI
+
+@main
+struct buzzrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
